ba973547b5162800f7567f50ac68b8040143b357
Brendo Maciel <Brendo Maciel@192.168.1.30>
2017-11-09 10:41:19 -0200

    Adicionado Position

--- src/Enum/DirectionEnum.java
+++ src/Enum/DirectionEnum.java
@@ -1,9 +1,24 @@
 package Enum;
 
 public enum DirectionEnum {
-	N(),
-	W(),
-	S(),
-	E();
+	N(0,1),
+	W(-1,0),
+	S(0,-1),
+	E(1,0);
+	
+	private int x;
+	private int y;
+	
+	DirectionEnum (int x, int y){
+		this.x = x;
+		this.y = y;
+	}
+	
+	public int getXDifference() {
+		return this.x;
+	}
+	public int getYDifference(){
+		return this.y;
+	}
 
 }
--- src/View/Main.java
+++ src/View/Main.java
@@ -1,4 +1,4 @@
-package Main;
+package View;
 
 public class Main {
 	public static void main(String[] args) {
