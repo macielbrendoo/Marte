e2036083f498857d84bd47fd0a1077d313fbd05a
Brendo Maciel <Brendo Maciel@192.168.1.30>
2017-11-09 11:26:14 -0200

    Criado comandos e adicionado rotação no enum.

--- src/Enum/DirectionEnum.java
+++ src/Enum/DirectionEnum.java
@@ -1,10 +1,7 @@
 package Enum;
 
 public enum DirectionEnum {
-	N(0,1),
-	W(-1,0),
-	S(0,-1),
-	E(1,0);
+	N(0,1), W(-1,0), S(0,-1), E(1,0);
 	
 	private int x;
 	private int y;
@@ -19,6 +16,34 @@ public enum DirectionEnum {
 	}
 	public int getYDifference(){
 		return this.y;
+	}
+	
+	public DirectionEnum getLeft(DirectionEnum dir) {
+		switch(dir) {
+			case N:
+				return DirectionEnum.W;
+			case W:
+				return S;
+			case S:
+				return E;
+			case E:
+				return N;
+		}
+		return null;
+	}
+	
+	public DirectionEnum getRight (DirectionEnum dir) {
+	switch (dir) {
+	case N:
+		return E;
+	case W:
+		return N;
+	case S:
+		return W;
+	case E:
+		return S;
+	}
+		return null;	
 	}
 
 }
