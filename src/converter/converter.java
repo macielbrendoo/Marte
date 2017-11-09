40fabdcd74d43db8999f6afd3d2c650cd3879839
Brendo Maciel <Brendo Maciel@192.168.1.30>
2017-11-09 12:08:58 -0200

    Converter adicionado

--- src/converter/converter.java
+++ src/converter/converter.java
@@ -1,5 +1,32 @@
 package converter;
 
-public class converter {
-
+public enum converter {
+	MOVE('m','M'),
+	LEFT('l','L'),
+	RIGHT('r','R');
+	
+	private char lower;
+	private char upper;
+	
+	converter (char lower, char upper){
+		this.lower = lower;
+		this.upper = upper;
+	}
+	
+	public char getLower() {
+		return lower;
+	}
+	
+	public char getUpper(){
+		return upper;
+	}
+	
+	public converter getCommand(char c) {
+		for(converter com : converter.values()) {
+			if(com.getLower() == c || com.getUpper() == c) {
+				return com;
+			}
+		}
+		return null;
+	}
 }
